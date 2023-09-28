550684c92282f2dae2222c8000446ad6e7ab07b6
josuabad <josu.abad11@outlook.es>
2023-09-28 12:05:20 +0200

    Update Blink.ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -20,7 +20,7 @@ void loop() {
   digitalWrite(verde, LOW);  // turn the LED off by making the voltage LOW
   Serial.println("Led verde apagado");
 
-  for (int i; i <= 3; i++) {
+  for (int i = 0; i < 3; i++) {
     digitalWrite(verde, HIGH);  // turn the LED on (HIGH is the voltage level)
     Serial.println("Led verde encendido");
     delay(1000);               // wait for a second
